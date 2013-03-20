a05875d7391c4042d2fb780ddb82f95fbdcea256
wahyu setianto <octopus.ws@gmail.com>
2013-03-21 05:29:20 +0700

    log delete, menuhin tempat

--- application/logs/log-2012-12-21.php
+++
@@ -1,4 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2012-12-21 05:30:25 --> Severity: Warning  --> mysql_pconnect() [<a href='function.mysql-pconnect'>function.mysql-pconnect</a>]: Access denied for user 'redcommb_mandala'@'localhost' (using password: YES) D:\project\php\lgmwash\system\database\drivers\mysql\mysql_driver.php 91
-ERROR - 2012-12-21 05:30:25 --> Unable to connect to the database
--- application/logs/log-2013-03-20.php
+++
@@ -1,3 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2013-03-20 20:06:55 --> 404 Page Not Found --> info
